3595ae5c77905e14888fc232e4988e064f02e1d2
zomint <zomint01@163.com>
2023-08-01 19:50:26 +0800

    update

--- test.ts
+++ test.ts
@@ -1,14 +1,10 @@
 emakefun.SpeechRecognizer.initialize(0x30)
-emakefun.SpeechRecognizer.addSpeechCommand(1, "xiao yi xiao yi")
-emakefun.SpeechRecognizer.addSpeechCommand(2, "bei jing")
-emakefun.SpeechRecognizer.addSpeechCommand(3, "chong qing")
-emakefun.SpeechRecognizer.addSpeechCommand(4, "cheng du")
-emakefun.SpeechRecognizer.addSpeechCommand(5, "shang hai")
-
-basic.showNumber(0)
-
-while (true) {
+emakefun.SpeechRecognizer.addSpeechCommand(1, "bei jing")
+emakefun.SpeechRecognizer.addSpeechCommand(2, "shang hai")
+emakefun.SpeechRecognizer.addSpeechCommand(3, "cheng du")
+emakefun.SpeechRecognizer.addSpeechCommand(4, "chong qing")
+basic.forever(function () {
   if (emakefun.SpeechRecognizer.isSpeechRecognized()) {
     basic.showNumber(emakefun.SpeechRecognizer.recognitionResult())
   }
-}
+})
