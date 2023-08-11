0005f1bf6d5e832ed9ddf4fcb43ff5cb9933e53a
zomint <zomint01@163.com>
2023-08-11 11:53:05 +0800

    update

--- speech_recognizer.ts
+++ speech_recognizer.ts
@@ -1,94 +1,101 @@
-//% weight=100 block="Emakefun"
-//% category="Emakefun" 
+//% block="Emakefun"
 namespace emakefun {
-  /**
-   * Speech recognizer blocks
-   */
-  export namespace SpeechRecognizer {
-    declare const enum Event {
-      RecognitionSuccess = 3
-    }
+  declare const enum Event {
+    RecognitionSuccess = 3
+  }
 
-    declare const enum CommandFlag {
-      CommandConsumed = 0,
-      CommandSendCompleted = 1,
-    }
+  declare const enum CommandFlag {
+    CommandConsumed = 0,
+    CommandSendCompleted = 1,
+  }
 
-    declare const enum Address {
-      Version = 0x00,
-      CommandFlag = 0x01,
-      Command = 0x03,
-      IdentificationIndex = 0x04,
-      ActivationMode = 0x04,
-      Timeout = 0x04,
-      IdentificationData = 0x05,
-      IdentificationLength = 0x37,
-      Event = 0x38,
-      Result = 0x39,
-    }
+  declare const enum Address {
+    Version = 0x00,
+    CommandFlag = 0x01,
+    Command = 0x03,
+    PhraseIndex = 0x04,
+    ActivationMode = 0x04,
+    Timeout = 0x04,
+    PhraseData = 0x05,
+    PhraseLength = 0x37,
+    Event = 0x38,
+    Result = 0x39,
+  }
 
-    declare const enum Command {
-      None = 0,
-      Reset = 1,
-      AddSpeechCommand = 2,
-      SetTrigger = 3,
-      SetTimeout = 4,
-    }
+  declare const enum Command {
+    None = 0,
+    Reset = 1,
+    AddSpeechCommand = 2,
+    SetTrigger = 3,
+    SetTimeout = 4,
+  }
 
-
-    let i2c_device_: emakefun.I2cDevice = undefined
+  export class SpeechRecognizer {
+    private readonly i2c_device: emakefun.I2cDevice = undefined
 
     /**
-     * Initialize the speech recognizer
-     * @param i2cAddress the I2C address, default 0x30
+     * Constructor
+     * @param i2c_address I2C address of the module, default 0x30
      */
-    //% block="speech recognizer initialize with i2c address $i2c_address" weight=100
-    //% i2c_address.defl=0x30
-    //% subcategory="SpeechRecognizer"
-    export function initialize(i2c_address: number = 0x30) {
-      i2c_device_ = new emakefun.I2cDevice(i2c_address)
-      waitCommandConsumed()
-      i2c_device_.writeByte(Address.Command, Command.Reset)
-      commandSendCompleted()
+    constructor(i2c_address: number = 0x30) {
+      this.i2c_device = new emakefun.I2cDevice(i2c_address)
+      this.waitCommandConsumed()
+      this.i2c_device.writeByte(Address.Command, Command.Reset)
+      this.commandSendCompleted()
     }
 
     /**
      * Add a new speech command
      * @param index the index to return when command is recognized
      * @param phrase the speech phrase to add
-     */
+    */
+    //% block="add speech command to $this|index = $index speech phrase = $phrase"
     //% subcategory="SpeechRecognizer"
-    //% block="add speech command $phrase|with index $index" weight=90
+    //% this.defl=speech_recognizer
+    //% weight=90
+    //% inlineInputMode=external
     //% index.min=0 index.max=255
-    export function addSpeechCommand(index: number, phrase: string) {
-      waitCommandConsumed()
-      i2c_device_.writeBytes(Address.Command, [Command.AddSpeechCommand, index])
+    addSpeechCommand(index: number, phrase: string) {
       const data = Buffer.fromUTF8(phrase)
-      i2c_device_.writeBytes(Address.IdentificationData, data)
-      i2c_device_.writeByte(Address.IdentificationLength, data.length)
-      commandSendCompleted()
+      this.waitCommandConsumed()
+      this.i2c_device.writeBytes(Address.Command, [Command.AddSpeechCommand, index])
+      this.i2c_device.writeBytes(Address.PhraseData, data)
+      this.i2c_device.writeByte(Address.PhraseLength, data.length)
+      this.commandSendCompleted()
     }
 
     /**
      * Get the recognized speech command index  
      */
-    //% block="speech recognition result"
+    //% block="|get speech recognition index of | phrase from $this"
     //% subcategory="SpeechRecognizer"
+    //% this.defl=speech_recognizer
     //% blockSetVariable=speech_recognition_result
-    export function result(): number {
-      if (i2c_device_.readByte(Address.Event) === Event.RecognitionSuccess) {
-        return i2c_device_.readByte(Address.Result);
+    //% inlineInputMode=external
+    result(): number {
+      if (this.i2c_device.readByte(Address.Event) === Event.RecognitionSuccess) {
+        return this.i2c_device.readByte(Address.Result);
       } else {
         return -1;
       }
     }
 
-    function waitCommandConsumed(): void {
-      while (i2c_device_.readByte(Address.CommandFlag) !== CommandFlag.CommandConsumed);
+    private waitCommandConsumed(): void {
+      while (this.i2c_device.readByte(Address.CommandFlag) !== CommandFlag.CommandConsumed);
     }
 
-    function commandSendCompleted(): void {
-      i2c_device_.writeByte(Address.CommandFlag, CommandFlag.CommandSendCompleted)
+    private commandSendCompleted(): void {
+      this.i2c_device.writeByte(Address.CommandFlag, CommandFlag.CommandSendCompleted)
     }
+  }
+
+  //% block="create speech recognizer|I2C address = $i2c_address"
+  //% subcategory="SpeechRecognizer"
+  //% blockSetVariable=speech_recognizer
+  //% i2c_address.defl=0x30
+  //% weight=100
+  //% inlineInputMode=external
+  export function createSpeechRecognizer(i2c_address: number = 0x30): SpeechRecognizer {
+    return new SpeechRecognizer(i2c_address)
   }
 }
--- test.ts
+++ test.ts
@@ -1,11 +1,22 @@
 let speech_recognition_result = 0
-emakefun.SpeechRecognizer.initialize(48)
-emakefun.SpeechRecognizer.addSpeechCommand(1, "bei jing")
-emakefun.SpeechRecognizer.addSpeechCommand(2, "shang hai")
-emakefun.SpeechRecognizer.addSpeechCommand(3, "cheng du")
-emakefun.SpeechRecognizer.addSpeechCommand(4, "chong qing")
+let speech_recognizer = emakefun.createSpeechRecognizer(
+  48
+)
+speech_recognizer.addSpeechCommand(
+  0,
+  "bei jing"
+)
+speech_recognizer.addSpeechCommand(
+  1,
+  "shang hai"
+)
+speech_recognizer.addSpeechCommand(
+  2,
+  "cheng du"
+)
 basic.forever(function () {
-  speech_recognition_result = emakefun.SpeechRecognizer.result()
+  speech_recognition_result = speech_recognizer.result(
+  )
   if (speech_recognition_result != -1) {
     basic.showNumber(speech_recognition_result)
   }
